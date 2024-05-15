c4a3a3ab5b14055c7deab9a0820564a0bd6be26f
Alejandra <matizestudio2@gmail.com>
2024-05-15 18:12:27 -0300

    avances en validacion de formulario

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,20 +1,59 @@
 const form = document.querySelector(".form");
+
 const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const errorContainer = document.querySelector("#errorContainer"); 
+const errorN = document.querySelector(".errorNombre"); 
+const errorE = document.querySelector(".errorEmail"); 
 
-
-form.addEventListener('input', (e) => {
-  
-})
+const enviado = document.querySelector("#enviado");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  if (nombre.value.trim().length >= 3) {
+  let errorValidacion = false;
+
+  if (nombre.value.trim().length > 3) {
   } else {
-    const error = document.querySelector(".error");
-    error.textContent = "Tenés que completar los campos obligatorios";
-    setTimeout(() => {
-      error.textContent = "";
-    }, 2000);
+    errorValidacion = true;
+    errorContainer.classList.add("mostrar-error");
+    errorN.textContent =
+      "El nombre y apellido son demasiado cortos. Por favor, asegúrate de ingresar al menos 3 caracteres.";
+
+    // setTimeout(() => {
+    //   errorN.textContent = "";
+    //   errorContainer.classList.remove("mostrar-error");
+    // }, 20000);
+  }
+
+  if (email.value.trim().length > 7) {
+  } else {
+    errorValidacion = true;
+    errorContainer.classList.add("mostrar-error");
+    errorE.textContent = "El correo electrónico ingresado no es válido. Por favor, asegúrate de escribir una dirección de correo electrónico válida.";
+
+    // setTimeout(() => {
+    //   errorE.textContent = "";
+    //   errorContainer.classList.remove("mostrar-error");
+    // }, 20000);   
+  }
+
+  if (!errorValidacion) {
+    enviado.style.display = "block";
+    enviado.textContent = "Hemos recibido tu solicitud de adopción. Nos pondremos en contacto contigo en los próximos días. ¡Gracias por darle a un perrito la oportunidad de tener una familia!";
+  }
+});
+
+nombre.addEventListener("input", () => {
+  if (nombre.value.trim().length > 3) {
+    errorN.textContent = "";
+    errorContainer.classList.remove("mostrar-error");
+  }
+});
+
+email.addEventListener("input", () => {
+  if (email.value.trim().length > 7) {
+    errorE.textContent = "";
+    errorContainer.classList.remove("mostrar-error");
   }
 });
--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,20 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const iconoNav = document.querySelector('.header__icono-nav');
-  const menuMobile = document.querySelector('.navegacion');
+document.addEventListener("DOMContentLoaded", () => {
+  const iconoNav = document.querySelector(".header__icono-nav");
+  const menuMobile = document.querySelector(".navegacion");
 
-  abrirMenu()
+  abrirMenu();
   function abrirMenu() {
-      iconoNav.addEventListener('click', (e) => {
-
-          if(e.target.src.includes('/assets/img/hamburguesa.svg')) {
-              iconoNav.src = '/assets/img/cruz-cerrar.svg';
-              menuMobile.classList.add('mostrar-menu-mobile');
-          
-          } else {
-              iconoNav.src = '/assets/img/hamburguesa.svg';
-              menuMobile.classList.remove('mostrar-menu-mobile');
-              
-          };       
-      });
-  };
+    iconoNav.addEventListener("click", (e) => {
+      if (e.target.src.includes("/assets/img/hamburguesa.svg")) {
+        iconoNav.src = "/assets/img/cruz-cerrar.svg";
+        menuMobile.classList.add("mostrar-menu-mobile");
+      } else {
+        iconoNav.src = "/assets/img/hamburguesa.svg";
+        menuMobile.classList.remove("mostrar-menu-mobile");
+      }
+    });
+  }
 });
