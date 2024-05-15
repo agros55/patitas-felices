d092ddf957146d989354c7c504706a5bd8d32f0c
Alejandra <matizestudio2@gmail.com>
2024-05-15 20:14:31 -0300

    validar formulario

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -3,8 +3,8 @@ const form = document.querySelector(".form");
 const nombre = document.querySelector("#nombre");
 const email = document.querySelector("#email");
 const errorContainer = document.querySelector("#errorContainer"); 
-const errorN = document.querySelector(".errorNombre"); 
-const errorE = document.querySelector(".errorEmail"); 
+let errorN = document.querySelector(".errorNombre"); 
+let errorE = document.querySelector(".errorEmail"); 
 
 const enviado = document.querySelector("#enviado");
 
@@ -13,29 +13,27 @@ form.addEventListener("submit", (event) => {
 
   let errorValidacion = false;
 
-  if (nombre.value.trim().length > 3) {
-  } else {
+  if (nombre.value.trim().length < 3 && nombre.value.trim().length > 0 ) {
     errorValidacion = true;
     errorContainer.classList.add("mostrar-error");
     errorN.textContent =
       "El nombre y apellido son demasiado cortos. Por favor, asegúrate de ingresar al menos 3 caracteres.";
-
-    // setTimeout(() => {
-    //   errorN.textContent = "";
-    //   errorContainer.classList.remove("mostrar-error");
-    // }, 20000);
+  } else if (nombre.value.trim().length === 0) { 
+    errorValidacion = true;
+    errorContainer.classList.add("mostrar-error");
+    errorN.textContent =
+      "El nombre y apellido es un campo requerido.";
   }
 
-  if (email.value.trim().length > 7) {
-  } else {
+  if (email.value.trim().length < 7 && nombre.value.trim().length > 0) {
     errorValidacion = true;
     errorContainer.classList.add("mostrar-error");
     errorE.textContent = "El correo electrónico ingresado no es válido. Por favor, asegúrate de escribir una dirección de correo electrónico válida.";
-
-    // setTimeout(() => {
-    //   errorE.textContent = "";
-    //   errorContainer.classList.remove("mostrar-error");
-    // }, 20000);   
+  } else if (email.value.trim().length === 0) {
+    errorValidacion = true; 
+    errorContainer.classList.add("mostrar-error");
+    errorE.textContent =
+      "El email es un campo requerido.";
   }
 
   if (!errorValidacion) {
