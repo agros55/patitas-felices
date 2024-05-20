676429169cc94e5056bf1eb5a1e1da94b6d5411c
agros55 <fernandoviveros95@gmail.com>
2024-05-20 12:48:26 -0300

    Fix: Icono menu mobile

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   abrirMenu();
   function abrirMenu() {
     iconoNav.addEventListener("click", (e) => {
-      if (e.target.src.includes("/assets/img/hamburguesa.svg")) {
-        iconoNav.src = "/assets/img/cruz-cerrar.svg";
+      if (e.target.src.includes("./assets/img/hamburguesa.svg")) {
+        iconoNav.src = "./assets/img/cruz-cerrar.svg";
         menuMobile.classList.add("mostrar-menu-mobile");
       } else {
-        iconoNav.src = "/assets/img/hamburguesa.svg";
+        iconoNav.src = "./assets/img/hamburguesa.svg";
         menuMobile.classList.remove("mostrar-menu-mobile");
       }
     });
